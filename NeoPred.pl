79d09fe701757da82d87d5fd2baa1bd0084f2b1e
fulongfei <lffu_0032@outlook.com>
2020-12-22 16:58:38 +0800

    add NeoPred.pl

--- NeoPred.pl
+++ NeoPred.pl
@@ -5,4 +5,103 @@ use File::Basename;
 use Data::Dumper;
 use FindBin qw/$Bin/;
 
-my ($name,$outdir,$vcf,)
+my ($name,$tvcf,$nbam,$outdir,$py2_bin,$py3_bin,$samtools,$bedtools,$ref);
+
+GetOptions(
+    "n:s" => \$name,                  # sample name         [Need]
+    "vcf:s" => \$tvcf,                # tumor vcf file      [Need] 
+    "nbam:s" => \$nbam,               # normal bam file     [Need]
+    "od:s" => \$outdir,               # output dir          [Need]
+    "py2:s" => \$py2_bin,             # python2             [Default: /home/fulongfei/miniconda3/envs/py27/bin/python2]
+    "py3:s" => \$py3_bin,             # python3             [Default: /home/fulongfei/miniconda3/bin/python3]
+    "samtools:s" => \$samtools,       # samtools bin        [Default: /home/fulongfei/miniconda3/bin/samtools]
+    "bedtools:s" => \$bedtools,       # bedtools bin        [Default: /home/fulongfei/miniconda3/bin/bedtools]
+    "ref:s" => \$ref,                 # ref fasta           [Default: /data1/database/b37/human_g1k_v37.fasta]
+    ) or die;
+
+
+# use NeoPredPipe software to do tumor neoantigenes prediction
+
+# input file includes:
+    # 1. tumor vcf
+    # 2. normal bam (for HLA typing)
+
+# tools used:
+    # 1. NeoPredPipe (main software)
+    # 2. OptiType (for HLA typing)
+    # 3. Annovar (for annot VCF file)
+
+
+# check args
+if (not defined $name || not defined $tvcf || not defined $nbam || not defined $outdir){
+    die "please check you args spec\n";
+}
+
+# default values
+if (not defined $py2_bin){
+    $py2_bin = "/home/fulongfei/miniconda3/envs/py27/bin/python2";
+}
+
+if (not defined $py3_bin){
+    $py3_bin = "/home/fulongfei/miniconda3/bin/python3";
+}
+
+if (not defined $samtools){
+    $samtools = "/home/fulongfei/miniconda3/bin/samtools";
+}
+
+if (not defined $bedtools){
+    $bedtools = "/home/fulongfei/miniconda3/bin/bedtools";
+}
+
+if (not defined $ref){
+    $ref = "/data1/database/b37/human_g1k_v37.fasta";
+}
+
+
+
+# make result dir
+if (!-d "$outdir/$name"){
+    `mkdir $outdir/$name`;
+}
+
+
+my $runsh = "$outdir/$name/neo\.$name\.sh";
+open SH, ">$runsh" or die;
+
+# get HLA region's fq1 & fq2
+print SH "perl $Bin/extract_hla_reads_from_nbam.pl $nbam $name $ref $samtools $bedtools $outdir/$name\n";
+
+# do HLA typing
+my $fq1 = "$outdir/$name/$name\.hla.chr6region.1.fastq";
+my $fq2 = "$outdir/$name/$name\.hla.chr6region.2.fastq";
+print SH "$py2_bin $Bin/tools/OptiType-1.3.2/OptiTypePipeline.py -i $fq1 $fq2 --dna -v -o $outdir/$name -c $Bin/tools/OptiType-1.3.2/config.ini.example\n";
+
+# re-format HLA result
+`cp $outdir/$name/*/*_result.tsv $outdir/$name/hla.raw.result.tsv`;
+print SH "perl $Bin/reformat_HLA_typing_result.pl $$outdir/$name/hla.raw.result.tsv $name $outdir/$name\n";
+
+# do neo antigene prediction
+# make a dir to contain origin VCF (soft link)
+if (!-d "$outdir/$name/vcf"){
+    `mkdir $outdir/$name/vcf`;
+}
+
+`cd $outdir/$name/vcf`;
+`ln -s $vcf $name\.vcf`; # obey NeoPredPipe's VCF naming
+
+my $hla_result = "$outdir/$name\.hla.xls"; # re-formatted hla
+print SH "$py3_bin $Bin/tools/NeoPredPipe/NeoPredPipe.py -I $outdir/$name/vcf -H $hla_result -o $outdir/$name -n $name -c 0 -E 8 9 10\n";
+
+close SH;
+
+
+
+
+
+
+
+
+
+
+
