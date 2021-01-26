92873ee148136e31f5a6eb4c72ee52a0a49a6fe1
fulongfei <lffu_0032@outlook.com>
2021-01-26 17:22:45 +0800

    update

--- NeoPred.pl
+++ NeoPred.pl
@@ -5,13 +5,14 @@ use File::Basename;
 use Data::Dumper;
 use FindBin qw/$Bin/;
 
-my ($name,$tvcf,$nbam,$outdir,$py2_bin,$py3_bin,$samtools,$bedtools,$ref);
+my ($name,$tvcf,$nbam,$outdir,$col,$py2_bin,$py3_bin,$samtools,$bedtools,$ref);
 
 GetOptions(
     "n:s" => \$name,                  # sample name         [Need]
     "vcf:s" => \$tvcf,                # tumor vcf file      [Need] 
     "nbam:s" => \$nbam,               # normal bam file     [Need]
     "od:s" => \$outdir,               # output dir          [Need]
+    "col:i" => \$col,                 # not normal col in VCF after the FORMAT field      [Need]
     "py2:s" => \$py2_bin,             # python2             [Default: /home/fulongfei/miniconda3/envs/py27/bin/python2]
     "py3:s" => \$py3_bin,             # python3             [Default: /home/fulongfei/miniconda3/bin/python3]
     "samtools:s" => \$samtools,       # samtools bin        [Default: /home/fulongfei/miniconda3/bin/samtools]
@@ -70,7 +71,7 @@ my $runsh = "$outdir/$name/neo\.$name\.sh";
 open SH, ">$runsh" or die;
 
 # get HLA region's fq1 & fq2
-print SH "perl $Bin/extract_hla_reads_from_nbam.pl $nbam $name $ref $samtools $bedtools $outdir/$name\n";
+print SH "perl $Bin/bin/extract_hla_reads_from_nbam.pl $nbam $name $ref $samtools $bedtools $outdir/$name\n";
 
 # do HLA typing
 my $fq1 = "$outdir/$name/$name\.hla.chr6region.1.fastq";
@@ -79,7 +80,7 @@ print SH "$py2_bin $Bin/tools/OptiType-1.3.2/OptiTypePipeline.py -i $fq1 $fq2 --
 
 # re-format HLA result
 `cp $outdir/$name/*/*_result.tsv $outdir/$name/hla.raw.result.tsv`;
-print SH "perl $Bin/reformat_HLA_typing_result.pl $$outdir/$name/hla.raw.result.tsv $name $outdir/$name\n";
+print SH "perl $Bin/bin/reformat_HLA_typing_result.pl $$outdir/$name/hla.raw.result.tsv $name $outdir/$name\n";
 
 # do neo antigene prediction
 # make a dir to contain origin VCF (soft link)
@@ -91,12 +92,22 @@ if (!-d "$outdir/$name/vcf"){
 `ln -s $vcf $name\.vcf`; # obey NeoPredPipe's VCF naming
 
 my $hla_result = "$outdir/$name\.hla.xls"; # re-formatted hla
-print SH "$py3_bin $Bin/tools/NeoPredPipe/NeoPredPipe.py -I $outdir/$name/vcf -H $hla_result -o $outdir/$name -n $name -c 0 -E 8 9 10\n";
+print SH "$py3_bin $Bin/tools/NeoPredPipe/NeoPredPipe.py -I $outdir/$name/vcf -H $hla_result -o $outdir/$name -n $name -c $col -E 8 9 10\n";
 
 close SH;
 
 
 
+sub tumor_vcf_format_col{
+    # Columns of regions within vcf that are not normal within a multiregion vcf file after the format field.
+    # Example: 0 is normal in test samples, tumor are the other columns.
+
+    my ($vcf) = @_;
+    open IN, "$vcf" or die;
+    while (<IN>){
+        chomp;
+        if /
+    }
 
 
 
