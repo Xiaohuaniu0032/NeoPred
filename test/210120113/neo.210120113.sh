6bf7d1094be1b13a6c91fdbd23853e08528de369
fulongfei <lffu_0032@outlook.com>
2021-02-04 17:23:33 +0800

    update test run.sh

--- test/210120113/neo.210120113.sh
+++ test/210120113/neo.210120113.sh
@@ -4,7 +4,7 @@ cp /home/fulongfei/workdir/git_repo/NeoPred/test/210120113/*/*_result.tsv /home/
 perl /data1/workdir/fulongfei/git_repo/NeoPred/bin/reformat_HLA_typing_result.pl /home/fulongfei/workdir/git_repo/NeoPred/test/210120113/hla.raw.result.tsv 210120113 /home/fulongfei/workdir/git_repo/NeoPred/test/210120113
 cd /home/fulongfei/workdir/git_repo/NeoPred/test/210120113/vcf
 ln -s /data3/Projects/panel889/210122_A00679_0483_BHV2VYDSXY/03_var/210120113.snvIndel.somatic.vcf 210120113.vcf
-perl /data1/workdir/fulongfei/git_repo/NeoPred/bin/filter_tumor_vcf.pl -vcf /data3/Projects/panel889/210122_A00679_0483_BHV2VYDSXY/03_var/210120113.snvIndel.somatic.vcf -od /home/fulongfei/workdir/git_repo/NeoPred/test/210120113/vcf_clean
+perl /data1/workdir/fulongfei/git_repo/NeoPred/bin/filter_tumor_vcf.pl -vcf /data3/Projects/panel889/210122_A00679_0483_BHV2VYDSXY/03_var/210120113.snvIndel.somatic.vcf -od /home/fulongfei/workdir/git_repo/NeoPred/test/210120113/vcf_clean -t Tissue
 /home/fulongfei/miniconda3/bin/python3 /data1/workdir/fulongfei/git_repo/NeoPred/tools/NeoPredPipe/NeoPredPipe.py -I /home/fulongfei/workdir/git_repo/NeoPred/test/210120113/vcf_clean -H /home/fulongfei/workdir/git_repo/NeoPred/test/210120113/210120113.hla.xls -o /home/fulongfei/workdir/git_repo/NeoPred/test/210120113 -n 210120113 -c 0 -E 8 9 10
 perl /data1/workdir/fulongfei/git_repo/NeoPred/bin/parse_NetMHCPan_raw_result.SNV.InDel.pl -n 210120113 -t SNV -l 8 -d /home/fulongfei/workdir/git_repo/NeoPred/test -od /home/fulongfei/workdir/git_repo/NeoPred/test/210120113
 perl /data1/workdir/fulongfei/git_repo/NeoPred/bin/parse_NetMHCPan_raw_result.SNV.InDel.pl -n 210120113 -t InDel -l 8 -d /home/fulongfei/workdir/git_repo/NeoPred/test -od /home/fulongfei/workdir/git_repo/NeoPred/test/210120113
