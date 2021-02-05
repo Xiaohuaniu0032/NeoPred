5c4cc24f9d115d4a8a5492575a30087a94690718
fulongfei <lffu_0032@outlook.com>
2021-02-05 17:31:27 +0800

    add cal TNB V2

---
+++ bin/cal_TNB.v2.pl
@@ -0,0 +1,81 @@
+use strict;
+use warnings;
+
+
+############ Note ##################
+# this script is only used in Letu Beijing
+# for other users, please use general cal_TNB.pl script
+#
+# 2021/2/5
+####################################
+
+my ($neo_res,$tmb_res,$name,$bed,$outdir) = @ARGV;
+
+# check file exists
+die "can not find $neo_res\n" if (!-e $neo_res);
+die "can not find $tmb_res\n" if (!-e $tmb_res);
+
+# cal bed length
+my $cap_len = 0;
+open BED, "$bed" or die;
+while (<BED>){
+	chomp;
+	my @arr = split /\t/;
+	my $base_n = $arr[2] - $arr[1];
+	$cap_len += $base_n;
+}
+close IN;
+
+print "BED cap len (in bp) is: $cap_len\n";
+my $cap_len_M = sprintf "%.2f", $cap_len/1000000;
+print "BED cap len (in Mbp) is: $cap_len_M\n";
+
+
+# read tmb file
+my %var_tmb;
+open TMB, "$tmb_res" or die;
+while (<TMB>){
+	chomp;
+	next if /^\#/;
+	my @arr = split /\t/;
+	my $var = "$arr[1]\t$arr[2]\t$arr[4]\t$arr[5]"; # chr/pos/ref/alt
+	$var_tmb{$var} = 1;
+}
+close TMB;
+
+	
+# cal how much line
+my @line;
+open IN, "$neo_res" or die; # filter out variants that do not exist in tmb file
+<IN>;
+while (<IN>){
+	chomp;
+	my @arr = split /\t/;
+	my $var = "$arr[5]\t$arr[6]\t$arr[7]\t$arr[8]";
+	if (exists $var_tmb{$var}){
+		push @line, $arr[1];
+	}else{
+		print "$_ \[$var\] do not exists in $tmb_res file, please pay attentation\n";
+	}
+}
+close IN;
+
+my $neo_n = 0;
+my %line;
+for my $i (@line){
+	if (!exists $line{$i}){
+		$line{$i} = 1;
+		$neo_n += 1;
+	}
+}
+print "$name has $neo_n Uniq neo antigens\n";
+
+my $outfile = "$outdir/$name\.TNB.txt";
+open O, ">$outfile" or die;
+print O "Sample\tTNB\n";
+
+my $tnb = sprintf "%.2f", $neo_n/$cap_len_M;
+print "$name TNB (tumor neoantigens burden) is: $tnb\/M\n";
+print O "$name\t$tnb\/M\n";
+close O;
+
